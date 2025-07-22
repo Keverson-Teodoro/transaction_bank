46878ceddfe29f2b8a5cb6ef8b248c0967ff2eb9
Keverson-Teodoro <keversonteodoro55@gmail.com>
2025-07-22 15:47:33 -0300

    initializing project

---
+++ src/main/java/transaction/bank/transaction_bank/TransactionBankApplication.java
@@ -0,0 +1,13 @@
+package transaction.bank.transaction_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/transaction/bank/transaction_bank/TransactionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package transaction.bank.transaction_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
