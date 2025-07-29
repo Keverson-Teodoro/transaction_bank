55fe82b59b22e1ebdae06658f519ef1312011e59
Keverson-Teodoro <keversonteodoro55@gmail.com>
2025-07-29 16:18:32 -0300

    creating method to find user by document in user repository class

--- src/main/java/transaction/bank/transaction_bank/repository/UserRepository.java
+++ src/main/java/transaction/bank/transaction_bank/repository/UserRepository.java
@@ -3,7 +3,11 @@ package transaction.bank.transaction_bank.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import transaction.bank.transaction_bank.model.entitie.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserById (long id);
+
+    Optional<User> findByDocument(String document);
 }
